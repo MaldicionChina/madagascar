ec883864ad96a8bcb69e05fde66c9b1b8fc580a6
Alexis Enrique Rodríguez Gutiérrez <alexisrodgtz@gmail.com>
2016-11-10 16:35:05 -0500

    Bash Script to preconfigurate Madagascar. Added new TODO to the Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # madagascar
+
+
+
+
+
+#TODO:
+
+cofigure debian/watch file to autodownload source code of Madagascar. Problems of uscan using  sourceforge urls
---
+++ preSetupMadagascar-1.7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Download Madagascar-1.7
+wget http://downloads.sourceforge.net/project/rsf/madagascar/madagascar-1.7/madagascar-1.7.tar.gz -O madagascar-1.7.orig.tar.gz
+
+# untar and extract in madagascar folder
+tar -xzf madagascar-1.7.orig.tar.gz -C ./madagascar --strip-components=1
