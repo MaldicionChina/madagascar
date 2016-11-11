6eae1d4e4f2841dcbf56fb7cb235a8ee20f2fa98
root <root@383850eeb47b>
2016-11-11 01:07:45 +0000

    Added override_dh_auto_configure to work with --prefix. Minor fix: preSetupMadagascar.sh change '-' to '_' in madagascar-1.7 for untar

--- preSetupMadagascar-1.7.sh
+++ preSetupMadagascar-1.7.sh
@@ -4,4 +4,4 @@
 wget http://downloads.sourceforge.net/project/rsf/madagascar/madagascar-1.7/madagascar-1.7.tar.gz -O madagascar_1.7.orig.tar.gz
 
 # untar and extract in madagascar folder
-tar -xzf madagascar-1.7.orig.tar.gz -C ./madagascar --strip-components=1
+tar -xzf madagascar_1.7.orig.tar.gz -C ./madagascar --strip-components=1
